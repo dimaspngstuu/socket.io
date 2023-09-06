5a848389a22556360c34c4e8607c8754431505e5
dimaspngstuu123@gmail.com <dimaspngstuu123@gmail.com>
2023-09-07 05:36:59 +0700

    simple chat application

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-
+const http = require("http");
 
 const expressServer = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(expressServer,{
+    cors: {
+        origin: "*"
+    }
+});
 
-//configure a socket io
-const {Server} = require('socket.io');
-const io = new Server(expressServer);
+io.on("connection",function(socket){
+    console.log("new User added");
 
-io.on('connection', socket => {
-    console.log('New User connection')
-
-    socket.on('disconnect', () => console.log('User Disconnect'))
 })
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/index.html")
 })
+
 
 expressServer.listen(3000, () => {
-    console.log('Express server listening on port 3000');
+    console.log("This server running in PORT 3000")
 })
